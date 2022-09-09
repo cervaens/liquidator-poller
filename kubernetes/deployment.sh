0554f43daa706142b9db245ae877f98a55ade5a0
Nuno Cervaens <nuno.cervaens@hoistgroup.com>
2022-09-09 19:38:22 +0100

    feat: adding deployment.sh file

---
+++ kubernetes/deployment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "- Start deploying image version "$1
+echo "- Getting production environment file..."
+#aws s3 cp s3://arkana-liquidator/mainnet.env ~/envs/
+
+echo "- Updating environment variables..."
+kubectl delete secret prod-secrets
+kubectl create secret generic prod-secrets --from-env-file=/home/ubuntu/envs/mainnet.env
+
+echo "- Setting new app image, updating pods..."
+kubectl set image deployments/liquidator-deployment liquidator-poller=082576982398.dkr.ecr.ca-central-1.amazonaws.com/liquidator-poller:$1
